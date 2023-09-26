f0f08c446f3ffeda4614f2b52b3336b72e581c01
Guilherme Oliveira <guilhermefrancooliveira1994@gmail.com>
2023-09-26 16:57:16 -0300

    initial commit
    .

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  
+$('#btn-reset').click(function(){
+  $('form').slideUp();
+})
+
+$('header button').click(function(){
+  $('form').slideDown();
+})  
+
+$('form').on('submit', function(e) {
+  
+  e.preventDefault();
+  const adressNewImg = $('#adress-new-img').val();
+  const newItem = $('<li style="display: none;"></li>');
+  $(`<img src="${adressNewImg}"/>`).appendTo(newItem);
+  $(`
+  <div class="overlay-img-link"/>
+    <a href="${adressNewImg}" target="_blank" title="Ver imagem em tamanho real">
+      Ver imagem em tamanho real
+    </a>
+  </div>
+  `).appendTo(newItem);
+  $(newItem).appendTo('ul');
+  $(newItem).fadeIn(1000);
+  $(adressNewImg).val('');
+})
+})
+
